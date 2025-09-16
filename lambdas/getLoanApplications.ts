10cb4ca0440d030f94a6b5f3f00b549111a04dbf
devarshiroy93 <devarshiroy93@gmail.com>
2025-09-16 12:09:30 +0530

    added ui and updated lamda for CORS

--- lambdas/getLoanApplications.ts
+++ lambdas/getLoanApplications.ts
@@ -38,7 +38,7 @@ export const handler = async (
     // Query LoanApplications GSI by userId
     const query = new QueryCommand({
       TableName: process.env.APPLICATIONS_TABLE!,
-      IndexName: "userId-index", // 👈 make sure this GSI exists
+      IndexName: "userid-index", // 👈 make sure this GSI exists
       KeyConditionExpression: "userId = :uid",
       ExpressionAttributeValues: {
         ":uid": { S: userId },
