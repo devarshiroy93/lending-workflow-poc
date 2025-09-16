c96e407152a821e18d37cecbccb3a5ab377550f0
devarshiroy93 <devarshiroy93@gmail.com>
2025-09-16 23:25:35 +0530

    ui build break fixed and deploy.yaml modified

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 import SubmitApplication from "./pages/SubmitApplication";
-import HowToUse from "./pages/howToUse";
+import HowToUse from "./pages/HowToUse";
 import ApplicationsList from "./pages/ApplicationsList";
 
 export default function App() {
